bde3515b06568613132e7a0d71ecabc3c75ace8f
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2023-09-16 20:10:05 +0530

    Add files via upload

---
+++ Loops/ForLoopExample.java
@@ -0,0 +1,18 @@
+public class ForLoopExample {
+
+    public static void main(String[] args) {
+
+        // Using a for loop to print numbers from 1 to 10
+        
+        // Start a for loop with an initial value of i = 1.
+        // Continue looping as long as i is less than or equal to 10.
+        // Increment i by 1 in each iteration.
+
+        for (int i = 1; i <= 10; i++) {
+            // Inside the loop, print the current value of i.
+            System.out.println(i);
+        }
+
+    }
+
+}
